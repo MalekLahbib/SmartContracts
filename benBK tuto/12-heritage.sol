af63756f8a924b9e99961b8cea4deea584f6a439
StudentS14 <lahbibmalek@hotmail.com>
2024-01-20 15:07:26 +0100

    benBK tuto + heritage

---
+++ benBK tuto/12-heritage.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.22;
+
+contract Owner {
+    
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier isOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
+}
+
+contract modifiers is Owner{
+    uint nombre;
+
+    function setNombre(uint _nombre) public isOwner{
+        nombre = _nombre;
+    }
+
+    function getNombre() public view isOwner returns(uint){
+        return nombre;
+    }
+}
